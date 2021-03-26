fe90d98c8bac8bcbb46e4005b910e9ccc37efc15
robinfleige <robin-fleige@web.de>
2021-03-26 12:31:56 +0100

    Changed the State creation to a smaller and more efficient state

--- agent_code/dqn/state.py
+++ agent_code/dqn/state.py
@@ -17,64 +17,130 @@ def state_to_features(game_state: dict) -> np.array:
     # This is the dict before the game begins and after it ends
     if game_state is None:
         return None
+    
+    
+    
 
     # For example, you could construct several channels of equal shape, ...
     channels = []
 
     # chanels consist of: 
-    #       - walls
-    #       - crates
+    #       - movable direction
+    #       - danger
     #       - coins
     #       - players
-    #       - bombs
+    #       - bomb strnegth
+    
+    # entries in chanels are for top, right, down, left in that order
+    
+    # prepare data
+    player_position = np.array([game_state["self"][3][0],game_state["self"][3][1]])
+    
+    adjusted_map = np.array(game_state["field"])
+    adjusted_map[player_position[0]][player_position[1]] = -2
+    print("Adjusted Map")
+    print(adjusted_map)
+    # Player = -2
+    # Wall = -1
+    # Crate = 1
+    # Free = 0
+    #TODO: Add Coins = 2
+    #TODO: Add Bombs = 3
+    #TODO: Add Others = 4
+    
+    
+    neighbouring_fields = []
+    neighbouring_fields.append((player_position[0]-1,    player_position[1]     ))
+    neighbouring_fields.append((player_position[0],      player_position[1]+1   ))
+    neighbouring_fields.append((player_position[0]+1,    player_position[1]     ))
+    neighbouring_fields.append((player_position[0],      player_position[1]-1   ))
+    print("Neighbouring Fields")
+    print(neighbouring_fields)
+    
+    # is it possible to move in that direction?
+    movable = np.zeros(4)
+    for i, position in enumerate(neighbouring_fields):
+        field = adjusted_map[int(position[0])][int(position[1])]
+        movable[i] = field == 0 or field == 2
+    print("Moveable")
+    print(movable)
+    
+    # is the field dangered by a bomb? if yes, how many turn until explosion?
+    dangered = np.zeros(4)
+    for i, position in enumerate(neighbouring_fields):
+        dangered[i] = 4
+        for bomb in game_state["bombs"]:
+            if get_dangered_fields_by_bomb(bomb[0],adjusted_map.shape).contains(position):
+                if dangered[i] > bomb[1]:
+                        dangered = bomb[1]
+    print("Dangered")
+    print(dangered)
+    
+    # distance to the next coin
+    coin_distance = np.zeros(4)
+    for i, position in enumerate(neighbouring_fields):
+        coin_distance[i] = get_closest(position,adjusted_map, 2)
+    print("Coin Distance")
+    print(coin_distance)
+                    
+    # distance to the next enemy
+    enemy_distance = np.zeros(4)
+    for i, position in enumerate(neighbouring_fields):
+        enemy_distance[i] = get_closest(position,adjusted_map, 4)
+    print("Enemy Distance")
+    print(enemy_distance)
 
-    # create walls channel
-    wall_map = np.zeros_like(game_state["field"])
-    for x, y in zip(*np.where(game_state["field"] == -1)):
-        wall_map[x][y] = 1
-    channels.append(wall_map)
-
-    # create crates channel
-    crate_map = np.zeros_like(wall_map)
-    for x, y in zip(*np.where(game_state["field"] == 1)):
-        crate_map[x][y] = 1
-    channels.append(crate_map)    
-
-    # create coins channel
-    coin_map = np.zeros_like(wall_map)
-    for x, y in game_state["coins"]:
-        coin_map[x][y] = 1
-    channels.append(coin_map)
-
-    # create bomb channel
-    bomb_map = np.zeros_like(wall_map)
-    for x, y in zip(*np.where(game_state["explosion_map"] == 1)):
-        bomb_map[x][y] = 1
-        # bomb basically is a wall -> player can't move past it
-        wall_map[x][y] = 1
-
-    for (x, y), countdown in game_state["bombs"]:
-        bomb_map[x][y] = 1
-        for i in range(1, 4):
-            if x+i < len(bomb_map[x]) - 1:
-                bomb_map[x+i][y] = 1
-            if x-i > 0:
-                bomb_map[x-i][y] = 1
-            if y+i < len(bomb_map[:,y]) - 1:
-                bomb_map[x][y+i] = 1
-            if y-i > 0:
-                bomb_map[x][y-i] = 1
-
-    channels.append(bomb_map)
-
-    # create player channel
-    player_map = np.zeros_like(wall_map)
-    _, _, _, (x, y) = game_state["self"]
-    player_map[x][y] = 1
-    for _, _, _, (x, y) in game_state["others"]:
-        player_map[x][y] = -1
-    channels.append(player_map)
+    # number of crates in bomb range
+    bomb_strength = np.zeros(4)
+    for i, position in enumerate(neighbouring_fields):
+        dangered_fields = get_dangered_fields_by_bomb(game_state["self"][3],adjusted_map.shape)
+        count = 0
+        for field in dangered_fields:
+            if(adjusted_map[field[0]][field[1]]) == 1:
+                count = count + 1
+        bomb_strength[i] = count
+    print("Bomb Strength")
+    print(bomb_strength)    
+        
 
     # concatenate them as a feature tensor (they must have the same shape), ...
+    channels.append(movable)
+    channels.append(dangered)
+    channels.append(coin_distance)
+    channels.append(enemy_distance)
+    channels.append(bomb_strength)
     stacked_channels = np.stack(channels).astype(float)
     return stacked_channels
+
+def get_closest(position, adjusted_map, goal):
+    to_move = [(position[0],position[1],0)]
+    while len(to_move) > 0:
+        position = to_move.pop(0)
+        if position[0] > 0 and position[1] > 0 and position[0] < adjusted_map.shape[0] and position[1] <adjusted_map.shape[1]:
+             if adjusted_map[position[0]][position[1]] == goal:
+                 return position[2]+1
+             else:
+                 if adjusted_map[position[0]][position[1]] == 0 or adjusted_map[position[0]][position[1]] == 2:
+                     adjusted_map[position[0]][position[1]] = -1
+                     to_move.append((position[0]+1,position[1]  ,position[2]+1))
+                     to_move.append((position[0]-1,position[1]  ,position[2]+1))
+                     to_move.append((position[0]  ,position[1]+1,position[2]+1))
+                     to_move.append((position[0]  ,position[1]-1,position[2]+1))
+    return -1
+
+def get_dangered_fields_by_bomb(bomb,shape):
+    dangered_fields = []
+    for i in range(3):
+        position = (bomb[0]+i,bomb[1]  )
+        if position[0] > 0 and position[1] > 0 and position[0] < shape[0] and position[1] <shape[1]:
+            dangered_fields.append(position)
+        position = (bomb[0]-i,bomb[1]  )
+        if position[0] > 0 and position[1] > 0 and position[0] < shape[0] and position[1] <shape[1]:
+            dangered_fields.append(position)
+        position = (bomb[0]  ,bomb[1]+i)
+        if position[0] > 0 and position[1] > 0 and position[0] < shape[0] and position[1] <shape[1]:
+            dangered_fields.append(position)
+        position = (bomb[0]  ,bomb[1]-i)
+        if position[0] > 0 and position[1] > 0 and position[0] < shape[0] and position[1] <shape[1]:
+            dangered_fields.append(position)
+    return dangered_fields
